0b561ff1576ef9b5533d5a02712b168b2ac3dece
raythurman2386 <raymondthurman5@gmail.com>
2019-11-14 19:51:22 -0500

    initial jokes actions and jokes reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,3 +1,9 @@
+import {
+  FETCH_JOKES_START,
+  FETCH_JOKES_SUCCESS,
+  FETCH_JOKES_FAILED
+} from "../actions";
+
 export const initialState = {
   jokes: [],
   isLoading: false,
@@ -7,6 +13,26 @@ export const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_JOKES_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ""
+      };
+    case FETCH_JOKES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        jokes: action.payload,
+        error: ""
+      };
+    case FETCH_JOKES_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        jokes: [],
+        error: action.payload
+      };
     default:
       return state;
   }
