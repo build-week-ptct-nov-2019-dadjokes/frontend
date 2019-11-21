4e2d119a8aa6d0496ea54b06950995deaaee4097
raythurman2386 <raymondthurman5@gmail.com>
2019-11-20 19:18:55 -0500

    initial addjoke refactor

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -4,10 +4,10 @@ export const POST_JOKES_START = "POST_JOKES_START";
 export const POST_JOKES_SUCCESS = "POST_JOKES_SUCCESS";
 export const POST_JOKES_FAILED = "POST_JOKES_FAILED";
 
-export const postJokes = () => dispatch => {
+export const postJokes = value => dispatch => {
   dispatch({ type: POST_JOKES_START });
   axios()
     .post(`/api/jokes`)
-    .then(res => dispatch({ type: POST_JOKES_SUCCESS, payload: res.data }))
+    .then(res => dispatch({ type: POST_JOKES_SUCCESS, payload: value }))
     .catch(err => dispatch({ type: POST_JOKES_FAILED, payload: err.response }));
 };
