f06469cebc7df3405b089d54ac1d1ec438bcd505
raythurman2386 <raymondthurman5@gmail.com>
2019-11-24 20:29:57 -0500

    basic update implemented, 500 server error on put

--- src/components/JokeCard.js
+++ src/components/JokeCard.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
+import { Card, CardBody, CardTitle, CardText, Button, Input } from "reactstrap";
+import { putJokes } from "../actions/putAction";
 import { deleteJokes } from "../actions/deleteAction";
+import { useInput } from "../hooks/useInput";
 
 function JokeCard(props) {
   const dispatch = useDispatch();
+  const [joke, handleJokeChanges] = useInput(props.joke.joke);
+  const [punchline, handlePunchlineChanges] = useInput(props.joke.punchline);
+  const [isEditing, setIsEditing] = useState(false);
 
   const loggedIn = localStorage.getItem("token");
 
@@ -12,12 +17,42 @@ function JokeCard(props) {
     <div>
       <Card key={props.joke.id}>
         <CardBody>
-          <CardTitle>{props.joke.joke}</CardTitle>
-          <CardText>{props.joke.punchline}</CardText>
+          {isEditing ? (
+            <Input
+              type="text"
+              value={joke}
+              onChange={() => handleJokeChanges()}
+            />
+          ) : (
+            <CardTitle>{joke}</CardTitle>
+          )}
+          {isEditing ? (
+            <Input
+              type="text"
+              value={punchline}
+              onChange={() => handlePunchlineChanges()}
+            />
+          ) : (
+            <CardText>{punchline}</CardText>
+          )}
+          {isEditing && (
+            <Button
+              onClick={() => {
+                dispatch(putJokes(props.joke.id));
+                setIsEditing(false);
+              }}
+            >
+              Save
+            </Button>
+          )}
         </CardBody>
         {loggedIn && (
           <>
-            <Button color="primary" style={{ marginBottom: "5px" }}>
+            <Button
+              color="primary"
+              style={{ marginBottom: "5px" }}
+              onClick={() => setIsEditing(true)}
+            >
               Update
             </Button>
             <Button
