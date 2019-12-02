a7de5837bea42231fee27cd527c5faaeda102c7e
raythurman2386 <raymondthurman5@gmail.com>
2019-12-01 20:44:28 -0500

    refactored register to hooks

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,103 +1,72 @@
-import React from 'react';
-import { Input, Label, Button, Wrapper, Title, SubmitButton } from './styled/index';
+import React from "react";
+import { useInput } from "../hooks/useInput";
+import {
+  Input,
+  Label,
+  Button,
+  Wrapper,
+  Title,
+  SubmitButton
+} from "./styled/index";
 
+const Register = () => {
+  const [username, handleUsername] = useInput("");
+  const [email, handleEmail] = useInput("");
+  const [password, handlePassword] = useInput("");
 
-export default class Register extends React.Component {
-  constructor() {
-    
-    super();
-
-    this.state = {
-      hasValidEmail: true,
-      hasSignedUp: false,
-      hasValidPassword: true,
-      
+  const onSubmit = e => {
+    e.preventDefault();
+    let user = {
+      username,
+      email,
+      password
     };
-  }
+    console.log(user);
+  };
 
-  componentDidMount() {
-//         this.emailInput.focus();
-  }
-  
-  shouldComponentUpdate(nextProps, nextState) {
-    
-  }
+  return (
+    <Wrapper>
+      <Title>Drop Block Signup</Title>
 
-  onBlur = evt => {
-    const isValidInput = this.validateInput(evt.target);
-    const inputType = evt.target.type;
-    
-    if (inputType === 'email') {
-      this.setState({hasValidEmail: isValidInput});
-    } else if (inputType === 'password') {
-      this.setState({hasValidPassword: isValidInput});
-    }
-  }
-  
-  onSubmit = evt => {
-    const { hasValidEmail, hasValidPassword } = this.state;
-    
-    evt.preventDefault();
-  }
-  
-  validateInput = input => {
-    const type = input.type;
-    let hasValidEmail = this.state.hasValidEmail;
-    let hasValidPassword = this.state.hasValidPassword;
-    
-    return input.validity.valid
-  }
-  
+      <form onSubmit={e => onSubmit(e)}>
+        <Label>
+          Username
+          <Input
+            type="username"
+            name="username"
+            value={username}
+            onChange={e => handleUsername(e.target.value)}
+            required
+          />
+        </Label>
 
-  render() {
-    var self = this;
-    return (
-      <Wrapper>
-        <Title>Drop Block Signup</Title>
+        <Label>
+          Email Address
+          <Input
+            label="Email Address"
+            type="email"
+            name="email"
+            value={email}
+            onChange={e => handleEmail(e.target.value)}
+            required
+          />
+        </Label>
 
-        <form onSubmit={this.onSubmit}>
-          <Label>
-            Username
-            <Input             
-              type="username"
-              pattern="[a-zA-Z]$"
-              onBlur={this.onBlur}
-              required
-            />
-          </Label>
-          
-          <Label>
-            Email Address
-            
-            <Input
-              className={`${!this.state.hasValidEmail ? 'invalid' : ''}`}
-              innerRef={function(input) { return self.emailInput = input}}
-              label="Email Address"
-              type="email"
-              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-              onBlur={this.onBlur}
-              required
-            />
-          </Label>
+        <Label>
+          Password (minimum 8 characters)
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            onChange={e => handlePassword(e.target.value)}
+            required
+          />
+        </Label>
 
-          <Label>
-            Password (minimum 8 characters)
-            
-            <Input
-              className={`${!this.state.hasValidPassword ? 'invalid' : ''}`}
-              innerRef={input => this.passwordInput = input}
-              onBlur={this.onBlur}
-              pattern="^[0-9a-zA-Z]{8,}$"
-              type="password"
-              required
-            />
-          </Label>
+        <SubmitButton shadow>Sign up</SubmitButton>
+      </form>
+    </Wrapper>
+  );
+};
 
-          <SubmitButton onSubmit={this.onSubmit} shadow>
-            Sign up
-          </SubmitButton>
-        </form>
-      </Wrapper>
-    );
-  }
-}
+export default Register;
